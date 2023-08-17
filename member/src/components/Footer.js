81935ded36a0f49da57b0a6245d21ce5cba44596
apple <kej2971@gmail.com>
2023-08-17 19:52:47 +0900

    init

---
+++ member/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import footertextstyle from './usecomponents.module.css';
+import footerstyle from './usecomponents.module.css';
+
+export default function Footer() {
+  return (
+    <div className={footerstyle.footerstyle}>
+        <h5 class={footertextstyle.footertextstyle}>
+          경기도 성남시 수정수 대왕판교로 815 판교제2테크노밸리 기업지원허브 1층 메타버스 아카데미
+          <br/>
+          <br/>
+          TEL : 02-317-6144 / FAX : 02-317-6062 
+          <br/>
+          <br/>
+          Copyright(C)Team Sleep After Die all rights reserved.
+        </h5>
+    </div>
+  );
+}
