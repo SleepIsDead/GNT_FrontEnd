71772c8511387ce8a0780dd20fd152197ebea258
apple <kej2971@gmail.com>
2023-08-25 18:30:22 +0900

    feature 햄버거바, 로그인화면, 나의 여행 목록 페이지 구현

--- member/src/components/Footer.js
+++ member/src/components/Footer.js
@@ -4,8 +4,10 @@ import footerstyle from './usecomponents.module.css';
 
 export default function Footer() {
   return (
-    <div className={footerstyle.footerstyle}>
-        <h5 class={footertextstyle.footertextstyle}>
+    <div id='wrapper'>
+    <footer className={footerstyle.footerstyle}>
+      <img id="logo" src="../plane_airplane_icon.png" width="150px;" height="150px"></img>
+        <h5 className={footertextstyle.footertextstyle}>
           경기도 성남시 수정수 대왕판교로 815 판교제2테크노밸리 기업지원허브 1층 메타버스 아카데미
           <br/>
           <br/>
@@ -14,6 +16,7 @@ export default function Footer() {
           <br/>
           Copyright(C)Team Sleep After Die all rights reserved.
         </h5>
+    </footer>
     </div>
   );
 }
--- member/src/layouts/layouts.js
+++ member/src/layouts/layouts.js
@@ -1,5 +1,7 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import {Outlet} from 'react-router-dom'
+import Body from "../components/Body";
 
 import React from 'react'
 
@@ -7,6 +9,8 @@ export default function Layouts() {
   return (
     <>
       <Header/>
+      <Body/>
+      <Outlet/>
       <Footer/>
     </>
   )
