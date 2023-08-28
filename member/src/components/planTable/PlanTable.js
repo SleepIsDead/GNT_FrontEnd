3a69cc6cc3ee78357f3c1e62b05f92a49eab4d30
apple <kej2971@gmail.com>
2023-08-28 17:13:50 +0900

    로고 변경, 나의 여행

--- member/src/components/planTable/PlanTable.js
+++ member/src/components/planTable/PlanTable.js
@@ -1,28 +1,37 @@
 import React from 'react';
 import './PlanTable.css';
+import Header from '../Header'
+import Footer from '../Footer';
 
 const PlanTable = props => {
   const { headersName, children } = props;
 
   return (
-    <table className="plan-table">
-      <thead>
-        <tr>
-          {
-            headersName.map((item, index) => {
-              return (
-                <td className="plan-table-header-column" key={index}>{ item }</td>
-              )
-            })
-          }
-        </tr>
-      </thead>
-      <tbody>
-        {
-          children
-        }
-      </tbody>
-    </table>
+    <>
+      <Header/>
+      <div className="plan-table-container">
+        <div className="plan-table-header">나의 여행</div>
+        <table className="plan-table">
+          <thead>
+            <tr>
+              {
+                headersName.map((item, index) => {
+                  return (
+                    <td className="plan-table-header-column" key={index}>{ item }</td>
+                  )
+                })
+              }
+            </tr>
+          </thead>
+          <tbody>
+            {
+              children
+            }
+          </tbody>
+        </table>
+      </div>
+      <Footer/>
+    </>    
   )
 }
 
