1e28e936c2f6088fe1fd1f290459b72a8145c1ef
youyeul lee <a1s1a1@naver.com>
2023-08-28 14:16:07 +0900

    no message

--- member/src/components/googlemap/GoogleMap.js
+++ member/src/components/googlemap/GoogleMap.js
@@ -1,47 +1,141 @@
-import React, { useState } from 'react';
-import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
-
-const mapStyle = {
-  width: '100vw',
-  height: '100vh',
+import React, { useEffect, useRef } from 'react'
+import { GoogleMap, useLoadScript, MarkerF ,InfoWindow } from '@react-google-maps/api';
+const containerStyle = {
+  width: '1400px',
+  height: '780px'
 };
 
-function Map (){
-  const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: 'AIzaSyBnBcEFs_o9xPHgartVyWKBkhsM9-ztLYc',
+const center = {
+  lat: 37.5649867,
+  lng: 126.985575
+};
+
+const OPTIONS = {
+  minZoom: 4,
+  maxZoom: 18,
+}
+const markers = [];
+
+
+
+function MyComponent() {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: 'AIzaSyB8r1IYnO2ly8v6OMgx1q2ljunzS-buFlA',
+    libraries: ["places"]
   });
+  
+  const [map, setMap] = React.useState(null)
 
-  const [map, setMap] = useState(null);
-
+  const searchInput = useRef();
+  
   const onLoad = React.useCallback(function callback(map) {
-    const bounds = new google.maps.LatLngBounds(
-      new google.maps.LatLng(40.712216, -74.22655),
-      new google.maps.LatLng(40.74, -74.18)
-    );
+    const bounds = new window.google.maps.LatLngBounds(center);
     map.fitBounds(bounds);
+    const infoWindow = new window.google.maps.InfoWindow({
+      content: "",
+      disableAutoPan: true,
+    });
+    const input = searchInput.current;
+
+    const searchBox = new window.google.maps.places.SearchBox(input);
+
+    map.addListener("bounds_changed", () => {
+      searchBox.setBounds(map.getBounds());
+    });
+
+    
+    searchBox.addListener("places_changed", () => {
+      const places = searchBox.getPlaces();
+
+      if (places.length == 0) {
+        return;
+      }
+
+      // // Clear out the old markers.
+      // markers.forEach((marker) => {
+      //   marker.setMap(null);
+      // });
+      // markers = [];
+      
+      // For each place, get the icon, name and location.
+      const bounds = new window.google.maps.LatLngBounds();
+      console.log(places)
+      console.log(places[0])
+      console.log(places[0].formatted_address)
+      places.forEach((place) => {
+        if (!place.geometry || !place.geometry.location) {
+          console.log("Returned place contains no geometry");
+          return;
+        }
+        console.log(place.name)
+
+        
+        // const icon = {
+        //   url: place.icon,
+        //   size: new window.google.maps.Size(71, 71),
+        //   origin: new window.google.maps.Point(0, 0),
+        //   anchor: new window.google.maps.Point(17, 34),
+        //   scaledSize: new window.google.maps.Size(25, 25),
+        // };
+
+        // Create a marker for each place.
+        const marker = new window.google.maps.Marker({
+          map,
+          // icon,
+          title: place.name,
+          position: place.geometry.location,
+          contents : place.name
+        });
+
+        marker.addListener("click", () => {
+          infoWindow.setContent(place.name)
+          infoWindow.open({
+            anchor: marker,
+            map,
+          });
+        })
+
+
+        markers.push(marker );
+        
+        if (place.geometry.viewport) {
+          // Only geocodes have viewport.
+          bounds.union(place.geometry.viewport);
+        } else {
+          // bounds.extend(place.geometry.location);
+        }
+      });
+      map.fitBounds(bounds);
+    });
+
     setMap(map);
-  }, []);
+  }, [])
 
   const onUnmount = React.useCallback(function callback(map) {
-    setMap(null);
-  }, []);
+    setMap(null)
+  }, [])
+
+  
+
+  
 
   return isLoaded ? (
+    <>
+    {/* <input
+      type="text"
+      ref={searchInput}
+    /> */}
     <GoogleMap
-      mapStyle={mapStyle}
-      defaultZoom={12}
-    //   defaultCenter=
+      mapContainerStyle={containerStyle}
+      center={center}
       onLoad={onLoad}
       onUnmount={onUnmount}
+      options={OPTIONS}
     >
-      {locations.map(item => {
-        return <Marker key={item.name} position={item.location} />;
-      })}
+      {/* <MarkerF position={center}></MarkerF> */}
     </GoogleMap>
-  ) : (
-    <></>
-  );
-};
+    </>
+  ) : <></>
+}
 
-export default React.memo(Map);
+export default React.memo(MyComponent)
--- member/src/layouts/GoogleMap.js
+++ member/src/layouts/GoogleMap.js
@@ -13,7 +13,6 @@ export default function GoogleMap() {
       <Header/>
       <Body/>
       <Outlet/>
-      <Footer/>
     </>
   )
 }
